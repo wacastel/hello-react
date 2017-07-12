3ab6b78717c3366ea790e8570291c6cfe29fd339
William Castellano <williamc246@gmail.com>
2017-07-12 16:15:23 -0700

    Added webpack, babel and react starter project

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+/*
+    ./client/components/App.jsx
+*/
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello to all the people!</h1>
+      </div>);
+  }
+}
---
+++ client/index.js
@@ -0,0 +1,9 @@
+/*
+    ./client/index.js
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+console.log('*** rendering the App component! ***');
+ReactDOM.render(<App />, document.getElementById('root'));
